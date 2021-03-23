71d7b2c1d45255d28d86d90f969f79270fb538aa
uooh_Z <fly5148@naver.com>
2021-03-23 16:38:33 +0900

    g

--- src/main/java/com/naver/s3/member/MemberDAO.java
+++ src/main/java/com/naver/s3/member/MemberDAO.java
@@ -38,41 +38,13 @@ public class MemberDAO {
 		
 	}
 	
-//	public MemberDTO logIn(MemberDTO memberDTO) throws Exception {
-//		
-//		String user = "user01";
-//		String pw = "user01";
-//		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-//		String driver = "oracle.jdbc.driver.OracleDriver";
-//		
-//		Class.forName(driver);
-//		
-//		Connection con = DriverManager.getConnection(url, user, pw);
-//		
-//		String sql = "select * from member where id=? and pw =?";
-//		PreparedStatement st = con.prepareStatement(sql);
-//		
-//		st.setString(1,memberDTO.getID() );
-//		st.setString(2,memberDTO.getPW() );
-//
-//		ResultSet rs = st.executeQuery();
-//		
-//		if(rs.next()) {
-//			memberDTO.setNAME(rs.getString("NAME"));
-//			memberDTO.setEMAIL(rs.getString("EMAIL"));
-//			memberDTO.setMOBILE(rs.getString("PHONE"));
-//					}else {
-//		memberDTO=null;	
-//		}
-//		
-//		rs.close();
-//		st.close();
-//		con.close();
-//		
-//		return memberDTO;
+	public MemberDTO memberLogIn(MemberDTO memberDTO) throws Exception {
+		
+		return sqlSession.selectOne(NAMESPACE+".memberLogin",memberDTO);
+	
+	
 		 
 		
-		
 	}
-
+}
 
