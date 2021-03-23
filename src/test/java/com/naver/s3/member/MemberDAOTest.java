d25ab65baa82a3ee7142c41eff737a4693955e33
uooh_Z <fly5148@naver.com>
2021-03-23 14:33:44 +0900

    test

---
+++ src/test/java/com/naver/s3/member/MemberDAOTest.java
@@ -0,0 +1,14 @@
+package com.naver.s3.member;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MemberDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
