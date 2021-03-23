06fcc10cb52f255884ffc1e56d4e3095fe6ec8f3
uooh_Z <fly5148@naver.com>
2021-03-23 14:09:05 +0900

    d

---
+++ src/main/java/com/naver/s3/member/MemberDAO.java
@@ -0,0 +1,91 @@
+package com.naver.s3.member;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class MemberDAO {
+	
+	public int memberUpdate(MemberDTO memberDTO) throws Exception{
+		//id를 제외하고 나머지를 업데이트
+		
+	}
+	
+	public int memberDelete(MemberDTO memberDTO) throws Exception {
+		
+		
+		
+	}
+	
+	
+	public int memberJoin(MemberDTO memberDTO)throws Exception {
+		String user = "user01";
+		String pw = "user01";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		
+		Class.forName(driver);
+		
+		Connection con = DriverManager.getConnection(url, user, pw);
+		
+		String sql = "insert into member values(?,?,?,?,?)";
+		PreparedStatement st = con.prepareStatement(sql);
+		
+		st.setString(1, memberDTO.getID());
+		st.setString(2, memberDTO.getPW());
+		st.setString(3, memberDTO.getNAME());
+		st.setString(4, memberDTO.getMOBILE());
+		st.setString(5, memberDTO.getEMAIL());
+		
+		int result = st.executeUpdate();
+		
+		st.close();
+		con.close();
+		
+		return result;
+		
+	}
+	
+	public MemberDTO logIn(MemberDTO memberDTO) throws Exception {
+		
+		String user = "user01";
+		String pw = "user01";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		
+		Class.forName(driver);
+		
+		Connection con = DriverManager.getConnection(url, user, pw);
+		
+		String sql = "select * from member where id=? and pw =?";
+		PreparedStatement st = con.prepareStatement(sql);
+		
+		st.setString(1,memberDTO.getID() );
+		st.setString(2,memberDTO.getPW() );
+
+		ResultSet rs = st.executeQuery();
+		
+		if(rs.next()) {
+			memberDTO.setNAME(rs.getString("NAME"));
+			memberDTO.setEMAIL(rs.getString("EMAIL"));
+			memberDTO.setMOBILE(rs.getString("PHONE"));
+					}else {
+		memberDTO=null;	
+		}
+		
+		rs.close();
+		st.close();
+		con.close();
+		
+		return memberDTO;
+		 
+		
+		
+	}
+
+}
---
+++ src/main/java/com/naver/s3/member/MemberDTO.java
@@ -0,0 +1,44 @@
+package com.naver.s3.member;
+
+public class MemberDTO {
+	
+private String ID;
+private String PW;
+private String EMAIL;
+private String NAME;
+private String MOBILE; 
+
+public String getID() {
+	return ID;
+}
+public void setID(String iD) {
+	ID = iD;
+}
+public String getPW() {
+	return PW;
+}
+public void setPW(String pW) {
+	PW = pW;
+}
+public String getEMAIL() {
+	return EMAIL;
+}
+public void setEMAIL(String eMAIL) {
+	EMAIL = eMAIL;
+}
+public String getNAME() {
+	return NAME;
+}
+public void setNAME(String nAME) {
+	NAME = nAME;
+}
+public String getMOBILE() {
+	return MOBILE;
+}
+public void setMOBILE(String mOBILE) {
+	MOBILE = mOBILE;
+}
+
+
+
+}
