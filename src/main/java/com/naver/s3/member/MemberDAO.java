1dd5416d6d5f87a6915e69cfe86fc00087cdf467
uooh_Z <fly5148@naver.com>
2021-03-23 15:05:35 +0900

    delete

--- src/main/java/com/naver/s3/member/MemberDAO.java
+++ src/main/java/com/naver/s3/member/MemberDAO.java
@@ -6,12 +6,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public class MemberDAO {
 	
+	@Autowired
 	private SqlSession sqlSession;
 	private final String NAMESPACE = "com.naver.s3.member.MemberDAO";
 	
@@ -22,76 +24,77 @@ public class MemberDAO {
 		
 	}
 	
-	//public int memberDelete(MemberDTO memberDTO) throws Exception {
+	public int memberDelete(MemberDTO memberDTO) throws Exception {
 		
-		
-		
-	//}
-	
-	
-	public int memberJoin(MemberDTO memberDTO)throws Exception {
-		String user = "user01";
-		String pw = "user01";
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String driver = "oracle.jdbc.driver.OracleDriver";
-		
-		Class.forName(driver);
-		
-		Connection con = DriverManager.getConnection(url, user, pw);
-		
-		String sql = "insert into member values(?,?,?,?,?)";
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		st.setString(1, memberDTO.getID());
-		st.setString(2, memberDTO.getPW());
-		st.setString(3, memberDTO.getNAME());
-		st.setString(4, memberDTO.getMOBILE());
-		st.setString(5, memberDTO.getEMAIL());
-		
-		int result = st.executeUpdate();
-		
-		st.close();
-		con.close();
-		
-		return result;
-		
-	}
-	
-	public MemberDTO logIn(MemberDTO memberDTO) throws Exception {
-		
-		String user = "user01";
-		String pw = "user01";
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String driver = "oracle.jdbc.driver.OracleDriver";
-		
-		Class.forName(driver);
-		
-		Connection con = DriverManager.getConnection(url, user, pw);
-		
-		String sql = "select * from member where id=? and pw =?";
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		st.setString(1,memberDTO.getID() );
-		st.setString(2,memberDTO.getPW() );
-
-		ResultSet rs = st.executeQuery();
-		
-		if(rs.next()) {
-			memberDTO.setNAME(rs.getString("NAME"));
-			memberDTO.setEMAIL(rs.getString("EMAIL"));
-			memberDTO.setMOBILE(rs.getString("PHONE"));
-					}else {
-		memberDTO=null;	
-		}
-		
-		rs.close();
-		st.close();
-		con.close();
-		
-		return memberDTO;
-		 
+		return sqlSession.delete(NAMESPACE+".memberDelete",memberDTO);
 		
 		
 	}
+	
+	
+//	public int memberJoin(MemberDTO memberDTO)throws Exception {
+//		String user = "user01";
+//		String pw = "user01";
+//		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+//		String driver = "oracle.jdbc.driver.OracleDriver";
+//		
+//		Class.forName(driver);
+//		
+//		Connection con = DriverManager.getConnection(url, user, pw);
+//		
+//		String sql = "insert into member values(?,?,?,?,?)";
+//		PreparedStatement st = con.prepareStatement(sql);
+//		
+//		st.setString(1, memberDTO.getID());
+//		st.setString(2, memberDTO.getPW());
+//		st.setString(3, memberDTO.getNAME());
+//		st.setString(4, memberDTO.getMOBILE());
+//		st.setString(5, memberDTO.getEMAIL());
+//		
+//		int result = st.executeUpdate();
+//		
+//		st.close();
+//		con.close();
+//		
+//		return result;
+//		
+//	}
+//	
+//	public MemberDTO logIn(MemberDTO memberDTO) throws Exception {
+//		
+//		String user = "user01";
+//		String pw = "user01";
+//		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+//		String driver = "oracle.jdbc.driver.OracleDriver";
+//		
+//		Class.forName(driver);
+//		
+//		Connection con = DriverManager.getConnection(url, user, pw);
+//		
+//		String sql = "select * from member where id=? and pw =?";
+//		PreparedStatement st = con.prepareStatement(sql);
+//		
+//		st.setString(1,memberDTO.getID() );
+//		st.setString(2,memberDTO.getPW() );
+//
+//		ResultSet rs = st.executeQuery();
+//		
+//		if(rs.next()) {
+//			memberDTO.setNAME(rs.getString("NAME"));
+//			memberDTO.setEMAIL(rs.getString("EMAIL"));
+//			memberDTO.setMOBILE(rs.getString("PHONE"));
+//					}else {
+//		memberDTO=null;	
+//		}
+//		
+//		rs.close();
+//		st.close();
+//		con.close();
+//		
+//		return memberDTO;
+//		 
+//		
+//		
+//	}
 
 }
--- src/test/java/com/naver/s3/member/MemberDAOTest.java
+++ src/test/java/com/naver/s3/member/MemberDAOTest.java
@@ -12,13 +12,33 @@ public class MemberDAOTest extends MyAbstractTest {
 	@Autowired
 	private MemberDAO memberDAO;
 	
+//	@Test
+//	public void memberUpdateTest () throws Exception {
+//		MemberDTO memberDTO = new MemberDTO();
+//		
+//		memberDTO.setId("id10");
+//		memberDTO.setEmail("hihi@naver.com");
+//		memberDTO.setName("hihi");
+//		memberDTO.setPhone("01022334455");
+//		memberDTO.setPw("hihi");
+//		
+//		int result = memberDAO.memberUpdate(memberDTO);
+//		
+//		assertEquals(1, result);
+//		
+//	}
+	
 	@Test
-	public void memberUpdateTest () throws Exception {
+	public void memberDeleteTest() throws Exception{
+		MemberDTO memberDTO = new MemberDTO();
 		
-		memberDTO.setEmail("");
+		memberDTO.setId("id10");
+		int result = memberDAO.memberDelete(memberDTO);
 		
 		
 	}
+	
+	
 	
 
 }
