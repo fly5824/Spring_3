d3ab0e3e32b3eae9a8a8cc7892aff32b18be24f0
uooh_Z <fly5148@naver.com>
2021-03-23 14:34:18 +0900

    t

--- src/test/java/com/naver/s3/member/MemberDAOTest.java
+++ src/test/java/com/naver/s3/member/MemberDAOTest.java
@@ -3,12 +3,22 @@ package com.naver.s3.member;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class MemberDAOTest {
+import com.naver.s3.MyAbstractTest;
 
+public class MemberDAOTest extends MyAbstractTest {
+
+	@Autowired
+	private MemberDAO memberDAO;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void memberUpdateTest () throws Exception {
+		
+		memberDTO.setEmail("");
+		
+		
 	}
+	
 
 }
